079fc61d65d8d1559864b7c8cfb1b28687c30abe
chenqi <chenqi331700@163.com>
2022-04-15 17:36:38 +0800

    更新了Student类

--- TestModule/src/com/cq/Student.java
+++ TestModule/src/com/cq/Student.java
@@ -1,4 +1,18 @@
 package com.cq;
 
 public class Student {
+    private String name;
+    private int    age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void study(){
+        System.out.println(name+"正在学习");
+    }
 }
